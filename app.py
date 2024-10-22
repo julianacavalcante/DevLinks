1a45e2247fdd54b31038a8e10113f9242c24627b
Juliana Cavalcante <ju.santana.pereira@gmail.com>
2024-10-22 10:21:25 +0100

    :tada:

---
+++ app.py
@@ -0,0 +1,3 @@
+A = (input('Digite o 1º lado do triângulo:  '))
+B = (input('Digite o 2º lado do triângulo:  '))
+C = (input('Digite o 3º lado do triângulo:  '))
---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  // pegar a tag img
+  const img = document.querySelector("#profile  img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adcionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tiver sem light mode, manter imagem normal
+    img.setAttribute("src", "./assets/avatar.jpeg")
+  }
+
+  html.classList.toggle("light")
+}
